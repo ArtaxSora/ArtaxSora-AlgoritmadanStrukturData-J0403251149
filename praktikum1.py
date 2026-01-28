afaae125a2db92d4949025a5a0a0983ce47fa7dc
ArtaxSora <tauriqarya@gmail.com>
2026-01-28 14:45:01 +0700

    CobaCoba

--- praktikum1.py
+++ praktikum1.py
@@ -1,1 +1,8 @@
-import math
+#Praktikum 1 : Konsep adt dan file handling
+#latihan dasar 1a : membaca seluruh isi file
+
+#membuka file dengan mode read (r)
+
+with open("dataMahasiswa.txt", "r",encoding="utf-8") as file:
+    isi_file = file.read()
+    print(isi_file)
